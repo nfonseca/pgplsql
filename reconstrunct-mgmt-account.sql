8a3abdd15f76b6985f69fdcde506f9d5a6c731f3
Nelson Fonseca <nelson.chamusca.fonseca@gmail.com>
2017-10-04 14:08:07 +0100

    Working example v2

--- reconstrunct-mgmt-account.sql
+++ reconstrunct-mgmt-account.sql
@@ -7,20 +7,20 @@ AS $function$
 
 DECLARE
 
-	total_esx int := 3;
+	total_esx int := 300;
 	username varchar  := 'management';
 	password varchar := 'ac11b0c9bd5e82ff7eba633845eaf9865fdf1b7b9cafd013';
---	status varchar :='OK';
+	status varchar :='OK';
 
 BEGIN
 
---	FOR esx  IN 1 .. 1 LOOP
+	FOR esx  IN 1 .. block.total_esx LOOP
 
 
---		INSERT INTO MANAGEMENT_ACCOUNT  SELECT * FROM public.management_account_bck;
-		INSERT INTO MANAGEMENT_ACCOUNT (id,component,component_id,host,username,password,status ) VALUES (1,'ESXi',NULL,'hostname',block.username,'password','status');
 
---	END LOOP;
+		INSERT INTO MANAGEMENT_ACCOUNT (id,component,component_id,host,username,password,status ) VALUES (esx,'ESXi',NULL,'hostname',block.username,block.password,block.status);
+
+	END LOOP;
 
 
 
@@ -36,5 +36,5 @@ select public.changeme();
 
 
 
---truncate MANAGEMENT_ACCOUNT;
+truncate MANAGEMENT_ACCOUNT;
 
